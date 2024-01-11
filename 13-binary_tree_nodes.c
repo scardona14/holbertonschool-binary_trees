a1e8541b6aad09198a8e72701ddf0986eaff701a
DilanSoto <dilanso12@gmail.com>
2024-01-11 00:15:42 -0400

    Update 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -16,5 +16,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree->left != NULL || tree->right != NULL)
 		count = 1;
 
-	return (count + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+return (count + binary_tree_nodes(tree->left) +
+        binary_tree_nodes(tree->right));
 }
